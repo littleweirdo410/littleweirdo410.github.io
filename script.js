16f6de0cef169f8f26e874c5f50a77a99b68290a
Milena A. Guseva <milenchik.guseva@ya.ru>
2024-09-17 13:26:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("back-to-top");
+ 
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset &gt; 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+ 
+  backToTop.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
